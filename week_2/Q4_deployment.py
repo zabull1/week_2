9cbd1ab4cd8474fdc5058b094f290b123b009a52
AMINU LAWAL <49124407+zabull1@users.noreply.github.com>
2023-02-06 20:41:44 +0000

    Create Q4_deployment.py

---
+++ week_2/Q4_deployment.py
@@ -0,0 +1,17 @@
+from prefect.deployments import Deployment
+from Q1_etl_web_to_gcs import etl_web_to_gcs
+from prefect.filesystems import GitHub 
+
+storage = GitHub.load("zoom-github-week2")
+
+print(storage)
+
+deployment = Deployment.build_from_flow(
+     flow=etl_web_to_gcs,
+     name="Week 2 Question 4",
+     storage=storage,
+     entrypoint="week_2/Q4_etl_web_to_gcs.py:etl_web_to_gcs",
+     parameters= {})
+
+if __name__ == "__main__":
+    deployment.apply()
